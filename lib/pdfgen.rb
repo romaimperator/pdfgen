90461e170ce3049c4dd133bcdf5c95e50e038d93
Daniel Fox <romaimperator@gmail.com>
2018-04-17 22:39:02 -0500

    allows setting viewport and what media type to emulate

--- lib/pdfgen.rb
+++ lib/pdfgen.rb
@@ -11,13 +11,28 @@ MAKE_PDF_COMMAND = File.expand_path('../javascript_bin/make_pdf.js', __FILE__)
 class Pdfgen
   def initialize(html)
     @html = html
+    @viewport_options = nil
+    @emulate_media = nil
+  end
+
+  def set_viewport(viewport_options)
+    @viewport_options = viewport_options
+    self
+  end
+
+  def emulate_media(media_type)
+    @emulate_media = media_type
+    self
   end
 
   def to_pdf(opts = {})
+    stdin_options = { pdf_options: opts, current_path: Dir.pwd }
+    stdin_options = stdin_options.merge(viewport_options: @viewport_options) if @viewport_options
+    stdin_options = stdin_options.merge(emulate_media: @emulate_media) if @emulate_media
     file = Tempfile.new('input_html')
     file.write(@html)
     file.close
-    pdf_output, status = Open3.capture2(MAKE_PDF_COMMAND, file.path, stdin_data: opts.to_json)
+    pdf_output, status = Open3.capture2(MAKE_PDF_COMMAND, file.path, stdin_data: stdin_options.to_json)
     file.unlink
     unless status.success?
       raise 'There was an unknown error running node to create the pdf. Check your logs for output that might assist in debugging.'
@@ -28,3 +43,6 @@ class Pdfgen
     pdf_output
   end
 end
+
+
+# Use a new API that uses method chaining to configure the PDF and that queues up strings to put in a js file to run puppeteer
